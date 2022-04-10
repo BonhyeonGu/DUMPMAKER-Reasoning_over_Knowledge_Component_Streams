91d94a5bf45c04ad83e00353c9a729b50dab7792
9Bon <tayasriel@gmail.com>
2022-04-10 21:21:39 +0900

    예비타이틀 첫글자 대문자로 변환

--- p2.py
+++ p2.py
@@ -17,34 +17,39 @@ def urlToTitle(keyword):
             tag = soup.select_one('#firstHeading')
             return tag.text
         except Exception as e:
-            print(keyword + " error")
+            print("\n" + keyword + " error")
     return -1
 
 MAXID = 70355200
 print("Start..")
+#arr = g.create_dataset('idToTitle', data=np.full(MAXID, '???',dtype=object))
+
 target = h5.File("./Dump0410.hdf5", 'r+')
 target.create_group('Ankers')
 g = target['Ankers']
-#arr = g.create_dataset('idToTitle', data=np.full(MAXID, '???',dtype=object))
-f1 = open("./Raw/02Ankers", 'r', encoding='UTF-8')
-f2 = open("./Raw/03Redirects_dict", 'r', encoding='UTF-8')
-f3 = open("./Raw/02Ankers_Title_ID", 'r', encoding='UTF-8')
 titleToId_dict = ast.literal_eval(target['/Titles'].attrs['titleToId'])
-print("Load f1 complite..")
-redirect_dict = ast.literal_eval(f2.read())
-print("Load f2 complite..")
-f2.close()
+print("Load Target complite..")
 
+f1 = open("./Raw/02Ankers", 'r', encoding='UTF-8')
 pages = f1.read().split('<')
-print("Page split complite..")
-for i in range(1, 11):
+f1.close()
+print("Load f1 complite..")
+
+f2 = open("./Raw/03Redirects_dict", 'r', encoding='UTF-8')
+redirect_dict = ast.literal_eval(f2.read())
+f2.close()
+print("Load f2 complite..")
+
+f3 = open("./Raw/02Ankers_Title_ID", 'r', encoding='UTF-8')
+for i in range(1, len(pages)):
     i_per = (float(i)/len(pages)) * 100
+
     lines = pages[i].split('\n')
-    
-    lineCount = len(lines) - 2#마지막 공백 라인 제외한 줄 갯수
     id = lines[0].split('=')[1].split('_')[0]#문자열임
     now = g.create_group(id)#가상경로
-    nowSize = lineCount - 1#제목까지 제외한 줄 갯수 == 엥커 갯수
+
+    lineCount = len(lines) - 2#파일을 읽을때 읽어야 할 줄 개수, 햇갈리니까 주의
+    nowSize = lineCount - 1#원소의 개수
     
     if nowSize == 0:
         now.attrs.create('size', data=nowSize)
@@ -53,7 +58,7 @@ for i in range(1, 11):
     anker_ids = now.create_dataset('anker_urls', data=np.full(nowSize, -1, dtype=np.int32))
 
     for j in range(1, lineCount):
-        print("\rProcess : p2 : %.4f%% : %.4f%%" % (i_per, (float(j)/len(lineCount)) * 100), end="")
+        print("\rProcess : p2 : %.4f%% : %.4f%%" % (i_per, (float(j)/lineCount) * 100), end="")
         words = lines[j].split('\t\t')
         anker_texts[j - 1] = words[1].encode('utf-8')
         des_id = f3.readline().split('\t\t')[0]
@@ -62,14 +67,17 @@ for i in range(1, 11):
             if des_id == -1:#존재하지 않으면
                 nowSize -= 1
                 continue
-            des_id = des_id.replace(' ', '_').encode('utf-8')
-            des_id = titleToId_dict[des_id]
+            des_id = des_id.replace(' ', '_')
+            des_id = des_id[0].capitalize() + des_id [1:]
+            des_id = des_id.encode('utf-8')
+            des_id = titleToId_dict[des_id]#문제사항!
         else:
             des_id = int(des_id)
         if des_id in redirect_dict:
             des_id = titleToId_dict[redirect_dict[des_id]]
+        anker_ids[j - 1] = des_id
     now.attrs.create('size', data=nowSize)
+
 target.close()
-f1.close()
 f3.close()
-input("All complite..")
+input("\nAll complite..")
